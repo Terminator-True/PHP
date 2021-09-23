ea3769f638a48ded801225b2dbbe7bfc5a097e01
Terminator-True <jf28renault@gmail.com>
2021-09-23 11:07:30 +0200

    001

---
+++ ex1.php
@@ -0,0 +1,26 @@
+<?php
+$string = "Hola";
+$numero_enter = 2;
+$booleana = TRUE;
+$numero_decimal = 2.3;
+$array = array("Joel","Farell","Cabrera");
+$res = null;
+
+//Objecte
+class Casa
+{
+    private $finestres = 5;
+    private $portes = 3;
+
+    function mostrar()
+    {
+        echo "Tinc una casa amb $this->finestres finestres i $this->portes portes"; 
+    }
+}
+
+$miCasa= new Casa;
+$miCasa->mostrar();
+
+
+?>
+
