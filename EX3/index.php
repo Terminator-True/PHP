ff31ec5bc11eb156d66460d4c063da24fc1ecc60
terminator-true <jf28renault@gmail.com>
2021-10-06 20:45:22 +0200

    020

---
+++ EX3/index.php
@@ -0,0 +1,45 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Imprimir per pantalla</title>
+</head>
+
+<?php 
+
+require "ex2.php";
+
+ImprimirCEU()
+?>
+<body>
+
+</body>
+
+<div id="Footer" style="background-color:turquoise;color:white;width: 100%;" align="center">
+<table>
+  
+    <tr>
+        <td colspan="4"> <div style="padding:20px; padding-bottom:30px"></div> </td>
+
+    </tr>
+    <tr>
+        <td colspan="4"> <hr width="100%"> </div> </td>
+
+    </tr>
+    
+    <tr style="color:white">
+        <td colspan="4" align="center">© 2020 EX3 | Todos los derechos reservados</td>
+    </tr>
+
+    <tr style="color:white">
+        <td colspan="4" align="center">
+            Nuestro correo electrónico : info@correo.com
+        </td>
+    </tr>
+
+</table>
+
+<div style="padding:0px; padding-bottom:30px"></div>  
+
+</div>
