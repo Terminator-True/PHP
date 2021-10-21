1e4f111b9896ad5868ec379596d838be3ed16970
Terminator-True <jf28renault@gmail.com>
2021-10-21 13:34:13 +0200

    023

--- EX4/ej1/ej1.php
+++ EX4/ej1/ej1.php
@@ -1,10 +1,41 @@
 <?php
+session_start();
+session_id();
+$_SESSION["user"]="Joel";
+
 if (isset($_POST["text"]) ){
-    echo "S'ha escrit";
-    header("http:/localhost/php-mp07/EX4/ej1/index.php");
+    $imprimir="S'ha escrit <br>";
+    $text=$_POST["text"];
+    $acabat=FALSE;
+    $i=0;
+    $j=0;
+    $pM=0;
+    while (!$acabat) {
+        if (is_numeric($text[$i])) {
+            $imprimir=$imprimir."Conté números";
+            $acabat=TRUE;
+        }
+        if ($i==strlen($text)-1 && !$acabat) {
+            $imprimir=$imprimir."No Conté números";
+            $acabat=TRUE;
+
+        }
+        if (count(explode(" ",$text))>1) {
+            $Paraules=preg_split(" ",$text);
+            if (strtoupper($Paraules[$j])==$Paraules[$j]) {
+                $pM++;
+            }
+            $j++;
+        }
+        $i++;
+    }
+    $imprimir=$imprimir."<br>Paraules en majúscules: $pM";
 }else {
-    
-    echo "No s'ha escrit";
+    $imprimir="No s'ha escrit";
 }
+
+$_SESSION["imprimir"]=$imprimir;
+#echo $_SESSION["imprimir"];
+header("Location: /php-mp07/EX4/ej1/index.php");
 
 ?>
--- EX4/ej1/index.php
+++ EX4/ej1/index.php
@@ -1,10 +1,11 @@
-
 <?php
-
-if (!isset($_SESSIO["user"])) {
-    ?><form action="ej1.php" method="post" >Text:<input type="text" name="text" ></form><?php
-}else
 session_start();
 $_SESSION["user"]="Joel";
+if (isset($_SESSION["user"])) {
+    printf($_SESSION["imprimir"]);
+
+}
+$_SESSION["imprimir"]="";
+?><form action="ej1.php" method="post" >Text:<input type="text" name="text" pattern="[A-Za-z0-9 ]{2,}" title="Invalid input"></form><?php
 
 ?>
