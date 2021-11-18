2569b6f5554dfa6de89fd1291b9a6cfd0ff0dd9b
Terminator-True <jf28renault@gmail.com>
2021-11-18 11:35:34 +0100

    032

---
+++ EX7/ex1/index.php
@@ -0,0 +1,9 @@
+<!-- Escriviu un script PHP per obtenir diferències entre dos dates (introduïdes en un formulari)  en anys, 
+mesos, dies, hores, minuts, segons. Verifiqueu que la primera data és més petita que la segona, sino mostra 
+error.
+-->
+
+<form action="ex1.php" method="post">
+    <input type="datetime" name="data1" id="">
+    <input type="submit" value="Submit">
+</form>
